e20c8ebeac9d7873f5c28719cd8317db6b0e67bb
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-06 11:39:31 +0100

    react-router-set-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import
+import { Navbar, Sidebar, Footer } from "./components";
+// import styled from 'styled-components'
+import {
+  AboutPage,
+  AuthWrapper,
+  CartPage,
+  CheckoutPage,
+  Error,
+  Home,
+  PrivateRoute,
+  ProductsPage,
+  SingleProductPage,
+} from "./pages";
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="products/:id" element={<SingleProductPage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import AboutPage from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import CartPage from "./CartPage";
+import CheckoutPage from "./CheckoutPage";
+import Error from "./ErrorPage";
+import Home from "./HomePage";
+import PrivateRoute from "./PrivateRoute";
+import ProductsPage from "./ProductsPage";
+import SingleProductPage from "./SingleProductPage"
+
+export {
+  AboutPage,
+  AuthWrapper,
+  CartPage,
+  CheckoutPage,
+  Error,
+  Home,
+  PrivateRoute,
+    ProductsPage,
+  SingleProductPage
+};
