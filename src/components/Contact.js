98be57ac09d5c0a69b0d5460d5418ce71c128f58
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-07 00:19:31 +0100

    home contact button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper>
+    <div className="section-center">
+      <h3>Join our newsletter and get 20% off</h3>
+      <div className="content">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore consectetur adipisci possimus eveniet at, nemo similique nobis corporis, porro facilis atque iste perspiciatis quia accusamus.</p>
+        <form className="contact-form">
+          <input type="email" className='form-input' placeholder='enter email' />
+          <button type='submit' className='submit-btn'>
+            subscribe
+          </button>
+        </form>
+      </div>
+    </div>
+  </Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
