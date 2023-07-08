80fae1188ac8b022beb7a1e2cb635d34c9c90e31
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-08 13:17:33 +0100

    star complete

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -3,13 +3,23 @@ import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
 const Stars = ({ stars, reviews }) => {
   console.log(stars, reviews)
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5;
+    return (
+      <span key={index}>
+        {stars >= index+1 ? (
+          <BsStarFill />
+        ) : stars > number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  })
   return <Wrapper>
     <div className="stars">
-      {/* star */}
-      <span>
-        
-      </span>
-      {/* end of star */}
+     {tempStars}
     </div>
     <p className="reviews">({reviews} customer reviews)</p>
   </Wrapper>
