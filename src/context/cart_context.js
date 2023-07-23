59a7f76d9cf69fea3ef93efce247085d12ab0a12
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-23 18:38:44 +0100

    cart column start

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,19 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
+
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"))
+  }
+  else {
+    return []
+  }
+}
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fees: 534,
@@ -29,7 +40,11 @@ export const CartProvider = ({ children }) => {
 
 }
   const toggleAmount = (id, value) => { }
-  const clearCart = () => {}
+  const clearCart = () => { }
+  
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart))
+  },[state.cart])
   return (
     <CartContext.Provider value={{...state, addToCart,removeItems,toggleAmount,clearCart}}>{children}</CartContext.Provider>
   )
