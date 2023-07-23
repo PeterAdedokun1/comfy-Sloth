d0f142674fd31c62ad6f27f632db511b4fffb8f0
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-23 18:44:59 +0100

    cart total ui

--- src/components/CartColumns.js
+++ src/components/CartColumns.js
@@ -2,9 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const CartColumns = () => {
-  return <Wrapper>
-    <div className="content"></div>
-  </Wrapper>
+  return (
+    <Wrapper>
+      <div className="content">
+        <h5>item</h5>
+        <h5>price</h5>
+        <h5>quantity</h5>
+        <h5>subtotal</h5>
+        <span></span>
+      </div>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
